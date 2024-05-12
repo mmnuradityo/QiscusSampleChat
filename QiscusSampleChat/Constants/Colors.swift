b84d167b6d7735d5344189bb9789721729415683
nuradityo <nuradityo@gmail.com>
2024-05-13 06:31:54 +0700

    refactor: manage asset

--- QiscusSampleChat/Constants/Colors.swift
+++ QiscusSampleChat/Constants/Colors.swift
@@ -8,13 +8,14 @@
 import UIKit
 
 struct Colors {
-  static let primaryColor = UIColor(hex: "#27B199")
-  static let strokeColor = UIColor(hex: "#E3E3E3")
-  static let chatFormBackgroundColor = UIColor(hex: "#FAFAFA")
-  static let textFormColor = UIColor(hex: "#B9B9B9")
-  static let textDefaultColor = UIColor(hex: "#232323")
-  static let datetTimeChatBackgroundColor = UIColor(hex: "#FFD97C")
-  static let bubleOtherColor = UIColor(hex: "#F7F7F7")
-  static let systemColor = UIColor(hex: "#01416C")
+  
+  static let primaryColor = UIColor(named: "PrimaryColor")!
+  static let strokeColor = UIColor(named: "StrokeColor")!
+  static let chatFormBackgroundColor = UIColor(named: "ChatFormBackgroundColor")!
+  static let textFormColor = UIColor(named: "TextFormColor")!
+  static let textDefaultColor = UIColor(named: "TextDefaultColor")!
+  static let datetTimeChatBackgroundColor = UIColor(named: "DatetTimeChatBackgroundColor")!
+  static let bubleOtherColor = UIColor(named: "BubleOtherColor")!
+  static let systemColor = UIColor(named: "SystemColor")!
   
 }
